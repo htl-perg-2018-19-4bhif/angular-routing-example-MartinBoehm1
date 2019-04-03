b120e3d073382ea779060f7408be8f8f7d5c3ab2
Martin <boehmmartin2000@yahoo.at>
2019-04-03 20:10:56 +0200

    homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './pokemons/pokemons.component';
+import { PokemonDetailsComponent } from './pokemons-details/pokemons-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent,
+    PokemonDetailsComponent
   ],
   imports: [
     BrowserModule,
